f865ac78919efda082b149aad00453735c198b81
Norro <andrewjm8989@gmail.com>
2025-05-10 19:01:26 -0600

    Added map

--- May-8th-9th/app.js
+++ May-8th-9th/app.js
@@ -1,6 +1,6 @@
-/*'use strict';
+'use strict';
 
-const switcher = document.querySelector('.btn');
+/*const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
@@ -15,3 +15,33 @@ switcher.addEventListener('click', function() {
 
     console.log('current class name: ' + className);
 });*/
+
+
+// Initialize the map
+const map = L.map('map').setView([20, 0], 2); // Default global view
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+// Add markers (replace with your own travel coordinates)
+const places = [
+  { name: "Edmonton Airport, Canada", coords: [50.302365, -113.577730] },
+  { name: "Calgary Airport, Canada", coords: [51.123463, -114.005797] },
+  { name: "Narita Airport, Japan", coords: [335.770082, 140.382287] },
+  { name: "Shinjuku Station, Tokyo", coords: [35.6895924, 139.7004131] }
+];
+
+const lineCoords = places.map(place => place.coords);
+L.polyline(lineCoords, {
+  color: 'blue',
+  weight: 3,
+  opacity: 0.7
+}).addTo(map);
+
+places.forEach(place => {
+    L.marker(place.coords).addTo(map).bindPopup(place.name);
+  });
